53a7c218a2081c733954357b691e8895a0fe8bba
estarace <eric.starace@gmail.com>
2024-10-27 23:46:26 -0500

    added player, asteroid, asteroidfields, and groups

---
+++ asteroid.py
@@ -0,0 +1,17 @@
+import pygame
+import circleshape
+from constants import *
+
+class Asteroid(circleshape.CircleShape):
+    def __init__(self,x,y,radius):
+        super().__init__(x,y,radius)
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self,screen):
+        pygame.draw.circle(screen,"white",self.position,self.radius,width=2)
+
+    def update(self,dt):
+        new_val=self.velocity*dt
+        self.position+=new_val
--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import pygame
 from constants import *
+import circleshape
+import player   
+import asteroid
+import asteroidfield
 
 def main():
     pygame.init()
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    x=SCREEN_WIDTH/2
+    y=SCREEN_HEIGHT/2
+    updatable=pygame.sprite.Group()
+    drawable=pygame.sprite.Group()
+    asteroids=pygame.sprite.Group()
+    player.Player.containers=(updatable,drawable)
+    asteroid.Asteroid.containers=(asteroids,updatable,drawable)
+    asteroidfield.AsteroidField.containers=(updatable)
+    aKiller=player.Player(x,y)
+    aField=asteroidfield.AsteroidField()
     
-    while 1:
+    while True:
+        clk=pygame.time.Clock()
+        dt=0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        dt=(clk.tick(60)/1000)
+        for i in updatable:
+            i.update(dt)
+        for z in drawable:
+            z.draw(screen)
         pygame.display.flip()
-    
+        
     
     print("\n")
     print(f"Starting asteroids!")
